548bfe7b5a10efd0094a9627b554256b16e2ae7e
sami ilyas nouicer <90932537+SamiIlyas-Nouicer@users.noreply.github.com>
2023-11-15 10:29:19 +0100

    Create exo2.5.c

---
+++ exo2.5.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "LinkedList.h"
+
+int main() {
+     printf("Linked List: ");
+    // Initialize an empty linked list
+    struct ListNode* myList = NULL;
+   
+
+    // Add elements to the linked list
+    addElement(&myList, 11);
+    addElement(&myList, 32);
+    addElement(&myList, 53);
+    addElement(&myList, 79);
+
+    
+    printf("link before circualr :");
+    displayList(myList);
+
+    circularList(myList);
+    printf("nombre de noeuds: %d\n", countNodes(myList));
+    //checkCircularList(myList);
+    
+
+    // Display the linked list
+    displayList(myList);
+  
+
+    // Free the memory allocated for the linked list
+    freeList(myList);
+
+    return 0;
+}
